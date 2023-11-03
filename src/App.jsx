0ce9cf41747e67d5663ef9513c9e9eb6d3617cf2
Zamua Halera <zamuahalera@mbp-de-zamua.lan>
2023-11-03 18:38:15 +0100

    flex on counter-container

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,6 @@ import Counter from "./components/Counter";
 function App() {
   const [items, setItems] = useState([0]);
 
-  //console.log("items.nbOfCounter.length =>", items.nbOfCounter.length);
-  // const array = [];
-  // for (let i = 0; i < item; i++) {
-  //   array.push(<Counter />);
-  // }
   console.log(items);
   return (
     <>
